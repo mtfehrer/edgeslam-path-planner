8cf690252eb66d886f5e762e1e97874177af73a9
Michael Fehrer <mtfehrer@gmail.com>
2025-06-20 12:40:03 -0400

    changes to docker setup

--- build-images.sh
+++ build-images.sh
@@ -1,14 +1,2 @@
-#Check if edgeslam image exists
-if ! docker image inspect edgeslam > /dev/null 2>&1; then
-    echo "Image 'edgeslam' not found. Building..."
-    docker build -t edgeslam -f Dockerfile ./edgeslam/
-else
-    echo "Image 'edgeslam' already exists."
-fi
-#Check if planner image exists
-if ! docker image inspect planner > /dev/null 2>&1; then
-    echo "Image 'planner' not found. Building..."
-    docker build -t planner -f Dockerfile ./planner/
-else
-    echo "Image 'planner' already exists."
-fi
+docker build -t edgeslam -f ./edgeslam/Dockerfile ./edgeslam/
+docker build -t planner -f ./planner/Dockerfile ./planner/
