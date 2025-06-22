37e540827b1fac9240cf7c096e65949e415ead72
Michael Fehrer <mtfehrer@gmail.com>
2025-06-22 19:10:12 -0400

    update docker config

--- start-mobile-container-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /opt/ros/melodic/setup.bash
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/edgeslam/Examples/ROS
-roscore &
-cd /home/edgeslam/Examples/ROS/Edge_SLAM/
-rosrun Edge_SLAM RGBD ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml client < /home/edgeslam/mobile-input.txt
-#rosbag play ./rgbd_dataset_freiburg2_desk.bag /camera/rgb/image_color:=/camera/rgb/image_raw /camera/depth/image:=/camera/depth_registered/image_raw
