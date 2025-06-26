b6f63adc38de25312b1ef0c21a1bdec78e49e64f
Michael Fehrer <mtfehrer@gmail.com>
2025-06-26 12:08:35 -0400

    add functions for updating occupancy grid

--- planner/main.cpp
+++ planner/main.cpp
@@ -6,9 +6,17 @@
 
 using namespace std;
 
-string mapPointsFilename = "/home/map_points.txt";
-string newestPoseFilename = "/home/newest_pose.txt";
-string allPosesFilename = "/home/all_poses.txt";
+const int GRID_SIZE = 50;
+const int VOXEL_SIZE = 1;
+
+// string mapPointsFilename = "/home/map-points.txt";
+// string newestPoseFilename = "/home/newest-pose.txt";
+// string allPosesFilename = "/home/all-poses.txt";
+
+//for testing without docker
+string mapPointsFilename = "/home/michael/Projects/edgeslam-path-planner/edgeslam/exported-data/map-points.txt";
+string newestPoseFilename = "/home/michael/Projects/edgeslam-path-planner/edgeslam/exported-data/newest-pose.txt";
+string allPosesFilename = "/home/michael/Projects/edgeslam-path-planner/edgeslam/exported-data/all-poses.txt";
 
 vector<vector<double>> loadMapPoints() {
     vector<vector<double>> points;
@@ -96,11 +104,46 @@ vector<vector<vector<double>>> loadAllPoses() {
     return poses;
 }
 
+void resetOccupancyGrid(vector<vector<vector<char>>>& occupancyGrid) {
+    occupancyGrid.clear();
+    for (int i = 0; i < GRID_SIZE; i++) {
+        vector<vector<char>> matrix;
+        for (int j = 0; i < GRID_SIZE; i++) {
+            vector<char> row;
+            for (int k = 0; i < GRID_SIZE; i++) {
+                row.push_back(0);
+            }
+            matrix.push_back(row);
+        }
+        occupancyGrid.push_back(matrix);
+    }
+}
+
+void addOccupiedVoxelsToOccupancyGrid(vector<vector<vector<char>>>& occupancyGrid, vector<vector<double>>& mapPoints) {
+    for (vector<double> p : mapPoints) {
+        occupancyGrid[p[0] / VOXEL_SIZE][p[1] / VOXEL_SIZE][p[2] / VOXEL_SIZE] = 1;
+    }
+}
+
+void addFreeVoxelsToOccupancyGrid(vector<vector<vector<char>>>& occupancyGrid, vector<vector<vector<double>>> allPoses) {
+    for (vector<vector<double>> p : allPoses) {
+        //calculate frustum
+        //calculate overlapping voxels
+    }
+    
+}
+
 int main() {
+    vector<vector<vector<char>>> occupancyGrid;
+
     while (1) {
         vector<vector<double>> mapPoints = loadMapPoints();
         vector<vector<double>> newestPose = loadNewestPose();
         vector<vector<vector<double>>> allPoses = loadAllPoses();
+
+        resetOccupancyGrid(occupancyGrid);
+        addOccupiedVoxelsToOccupancyGrid(occupancyGrid, mapPoints);
+        addFreeVoxelsToOccupancyGrid(occupancyGrid, allPoses);
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }
     return 0;
